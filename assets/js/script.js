75792d2ef41d48e611dac9757144ff5e7bfa7e3e
CW1224 <xixiwang1224@gmail.com>
2022-08-20 20:31:21 +0000

    Added in the necessary variables to the javascript section that will make the quiz more interactive.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,9 @@
+//Getting all the required items in the html file.
+let heading = getElementById('main-heading');
+let initialInformation = getElementById('provocation');
+const startButton = getElementById('start-quiz');
+const information = getElementById('information');
+
 //This is the code for the countdown timer. The timer counts down from 10 to 0 seconds with the milliseconds showing.
 
 const startingSeconds = 10;
