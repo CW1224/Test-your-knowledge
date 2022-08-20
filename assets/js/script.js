a2e7d4b45be6579c3086e1c0901921a3a01832d1
CW1224 <xixiwang1224@gmail.com>
2022-08-20 23:18:32 +0000

    Added in the javascript that would bring the user to the "next page" of the website. The option to exit and go back to the main page is also included.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,31 @@
 //Getting all the required items in the html file.
-let heading = getElementById('main-heading');
-let initialInformation = getElementById('provocation');
-const startButton = getElementById('start-quiz');
-const information = getElementById('information');
+let heading = document.getElementById('main-heading');
+let initialInformation = document.getElementById('provocation');
+const startButton = document.getElementById('start-quiz');
+const information = document.getElementById('information');
+const exitButton = document.getElementById('exit-quiz');
+const initiateButton = document.getElementById('begin-quiz');
+const quiz = document.getElementById('quiz');
+
+//This is the code that will bring the user to the quiz information page after pressing the start button.
+startButton.onclick = function() {
+    information.classList.add("activeInfo");
+    heading.classList.add("main-heading-remove");
+    initialInformation.classList.add("provocation-none");
+}
+
+//This is the code that would bring the user back to the initial page of the website.
+exitButton.onclick = function() {
+    information.classList.remove("activeInfo");
+    heading.classList.remove("main-heading-remove");
+    initialInformation.classList.remove("provocation-none");
+}
+
+//This is the code that would bring the user to the first question of the quiz when pressed.
+initiateButton.onclick = function() {
+    information.classList.remove("activeInfo"); //removes the information section
+    quiz.classList.add('quiz-active');
+}
 
 //This is the code for the countdown timer. The timer counts down from 10 to 0 seconds with the milliseconds showing.
 
