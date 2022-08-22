d710a8bcf65c6169544d6e376e92a3992036c165
CW1224 <xixiwang1224@gmail.com>
2022-08-22 11:39:19 +0000

    Added in the actual questions of the quiz. These would actually appear when the start and next buttons are pressed.

--- assets/js/questions.js
+++ assets/js/questions.js
@@ -3,18 +3,18 @@
 let questions = [
     {
         number:1,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What is the tallest mountain in our solar system?",
+        correctAns:"Everest",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Mount Everest",
+            "Carrauntoohil",
+            "Olympus Mons",
+            "Mount Kilimanjaro"
         ]
     },
     {
         number:2,
-        question:"How much is in a dozen?",
+        question:"How much is in a baker's dozen?",
         correctAns:12,
         options: [
             13,
@@ -25,255 +25,255 @@ let questions = [
     },
     {
         number:3,
-        question:"How old is Donald Trump?",
-        correctAns:76,
+        question:"After mating, what do female black widow spiders do?",
+        correctAns:"Eat their mate",
         options: [
-            74,
-            76,
-            78,
-            72
+            "Lay eggs",
+            "Eat their mate",
+            "Go out to hunt",
+            "Enjoy their mating gift"
         ]
     },
     {
         number:4,
-        question:"How strong is maria?",
-        correctAns:'Very',
+        question:"What is the smallest particle within in an atom?",
+        correctAns:'Electron',
         options: [
-            "Very",
-            "Not really",
-            "Barely",
-            "Strongest in the world"
+            "Proton",
+            "Neutron",
+            "Nucleus",
+            "Electron"
         ]
     },
     {
         number:5,
-        question:"What is her name?",
-        correctAns:'Lily',
+        question:"Who founded Apple?",
+        correctAns:"Steve Jobs, Ronald Wayne & Steve Wozniak",
         options: [
-            "Lily",
-            "Neasa",
-            "Philiip",
-            "Ryan"
+            "Steve Jobs, Ronald Wayne & Steve Wozniak",
+            "Stephen Jobs, Ronaldo Wyne & Steve Wozniak",
+            "Steve Jobs, Ronaldo Wyne & Steve Woznak",
+            "Stephen Jobs, Ronald Wayne & Steve Wozniak"
         ]
     },
     {
         number:6,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"Who is the richest man in the world in the year 2021?",
+        correctAns:"Jeff Bezos",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Elon Musk",
+            "Jeff Bezos",
+            "Bill Gates",
+            "Donald Trump"
         ]
     },
     {
         number:7,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"How many continents are there in the world?",
+        correctAns:7,
         options: [
-            13,
-            15,
-            16,
-            12
+            8,
+            9,
+            6,
+            7
         ]
     },
     {
         number:8,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What color flame is the hottest",
+        correctAns:"Blue",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Orange",
+            "Red",
+            "Blue",
+            "White"
         ]
     },
     {
         number:9,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"Which of the two gases caused the most deaths in World War 1?",
+        correctAns:"Mustard Gas",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Mustart Gas",
+            "Chlorine Gas",
+            "Both",
+            "Neither",
         ]
     },
     {
         number:10,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"Ireland's national holiday in held on what day of the year?",
+        correctAns:"March 17th",
         options: [
-            13,
-            15,
-            16,
-            12
+            "March 15th",
+            "March 17th",
+            "June 1st",
+            "June 15th"
         ]
     },
     {
         number:11,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What is the largest fish in the world?",
+        correctAns:"Whale Shark",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Great White Shark",
+            "Blue Whale",
+            "Whale Shark",
+            "Clownfish"
         ]
     },
     {
         number:12,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What is another name for Ayers Rock?",
+        correctAns:"Uluru",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Uluru",
+            "Kata Tjuṯa",
+            "Karlwe Karlwe",
+            "Olgas"
         ]
     },
     {
         number:13,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What is the name of Marie Curie's husband?",
+        correctAns:"Pierre Curie",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Piers Curie",
+            "Piero Curie",
+            "Pier Curie",
+            "Pierre Curie"
         ]
     },
     {
         number:14,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"Florence Nightingale's other name is:",
+        correctAns:"The Lady with the Lamp",
         options: [
-            13,
-            15,
-            16,
-            12
+            "The Nurse with the Lamp",
+            "The Nurse with the Light",
+            "The Lady with the Light",
+            "The Lady with the Lamp"
         ]
     },
     {
         number:15,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What class are frogs classified as?",
+        correctAns:"Amphibians",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Reptiles",
+            "Amphibians",
+            "Fish",
+            "Serpents"
         ]
     },
     {
         number:16,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What was the name of the aircraft that dropped the first atomic bomb in warfare?",
+        correctAns:"Enola Gay",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Bockscar",
+            "Enola Gay",
+            "Fat Man",
+            "Little Boy"
         ]
     },
     {
         number:17,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What was the name of the first atomic dropped in warfare?",
+        correctAns:"Little Boy",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Bockscar",
+            "Enola Gay",
+            "Fat Man",
+            "Little Boy"
         ]
     },
     {
         number:18,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"Who is the oldest of the Kardashian family?",
+        correctAns:"Kourtney Kardashian",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Kourtney Kardashian",
+            "Kylie Jenner",
+            "Kim Kardashian",
+            "Kendall Jenner"
         ]
     },
     {
         number:19,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What age was Tutankhamun when he died?",
+        correctAns:19,
         options: [
-            13,
-            15,
+            9,
+            19,
             16,
-            12
+            18
         ]
     },
     {
         number:20,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"Which of the old Seven Wonders of the World is still standing?",
+        correctAns:"Great Pyramids of Giza",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Great Pyramids of Giza",
+            "Hanging Gardens of Babylon",
+            "Lighthouse of Alexandria",
+            "Temple of Artemis"
         ]
     },
     {
         number:21,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What is the furthest planet from the Sun?",
+        correctAns:"Neptune",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Pluto",
+            "Neptune",
+            "Uranus",
+            "Jupiter"
         ]
     },
     {
         number:22,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What category does sugar fall into?",
+        correctAns:"Carbohydrates",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Salts",
+            "Vitamins",
+            "Fats",
+            "Carbohydrates"
         ]
     },
     {
         number:23,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What is the smallest ocean on Earth?",
+        correctAns:"Arctic Ocean",
         options: [
-            13,
-            15,
-            16,
-            12
+            "Southern Ocean",
+            "Arctic Ocean",
+            "Pacific Ocean",
+            "Indian Ocean"
         ]
     },
     {
         number:24,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"When did World War 2 start?",
+        correctAns:"September 1, 1939",
         options: [
-            13,
-            15,
-            16,
-            12
+            "September 1, 1945",
+            "September 2, 1945",
+            "September 1, 1939",
+            "September 2, 1939"
         ]
     },
     {
         number:25,
-        question:"How much is in a baker's dozen?",
-        correctAns:13,
+        question:"What is the age of Queen Elizabeth the second in the year 2021?",
+        correctAns:95,
         options: [
-            13,
-            15,
-            16,
-            12
+            96,
+            95,
+            94,
+            97
         ]
     },
 
